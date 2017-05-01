7b54eb79871a78a56d4a5ee17bf8cddf9935ff42
Michael Cho <Michael Cho>
2017-05-01 06:58:13 -0500

    updated heatmap files with comments

--- 05_heatmap_mnist_cnn.py
+++ 05_heatmap_mnist_cnn.py
@@ -1,110 +1,111 @@
+'''Trains a simple convnet on the MNIST dataset.
+
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
 from __future__ import print_function
 
+#%% ------ GPU memory fix -------
+import tensorflow as tf
+import keras.backend.tensorflow_backend as ktf
+
+def get_session(gpu_fraction=0.333):
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction,
+                                allow_growth=True)
+    return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+ktf.set_session(get_session())
+#%% ------ GPU memory fix -------
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout,BatchNormalization
-from keras.optimizers import RMSprop,Adagrad,SGD,Adadelta
-from keras.callbacks import EarlyStopping
-from keras.models import load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 
-import cv2
-#from matplotlib import pyplot as plt
-import pandas as pd
-import numpy as np
-import glob
-
-size = 56,56
-rows, columns = 56, 56
-n_images = 12500
-
-#function to read in and resize a color image
-def read_colimage(src):
-    im = cv2.imread(src, cv2.IMREAD_COLOR)
-    im = cv2.resize(im, (size), interpolation=cv2.INTER_NEAREST)
-    return im
-
-def read_data():
-    #read in images
-    print('reading images...')
-    imList = np.ndarray((n_images, rows, columns, 3), dtype=np.uint8)
-    x = 0
-    ct = 0
-    for infile in glob.glob('C:/Users/mwcho/Desktop/trainimages/screencaps_fighting/game_*/*.jpg'):
-#    for infile in glob.glob('C:/Users/Dylan/Documents/Northwestern/Spring 2017/Deep Learning/Deep_Learning_Project_LOCAL_FILES/screencaps_fighting/game_*/*.jpg'):
-        imList[ct] = read_colimage(infile)
-        ct += 1
-        if infile[-7:] == '500.jpg':
-            x += 1
-            print(x)
-    print('done reading images')
-    
-    #read in labels
-    print('reading labels...')
-    y = []
-    for infile in glob.glob('C:/Users/mwcho/Desktop/trainimages/screencaps_fighting/game_*/*.csv'):
-#    for infile in glob.glob('C:/Users/Dylan/Documents/Northwestern/Spring 2017/Deep Learning/Deep_Learning_Project_LOCAL_FILES/screencaps_fighting/game_*/*.csv'):
-        newY = pd.read_csv(infile, header=None, names=['stroke'])
-        newY = newY['stroke'].values.tolist()
-        y.extend(newY)
-    y = np.array(y)
-    print('done reading labels')
-    
-    return imList, y
-
-#%%
-#MAIN
 batch_size = 128
-num_classes = 4
-epochs = 20
+num_classes = 10
+epochs = 50
 
-x_all, y_all = read_data()
+# input image dimensions rows(height) columns (width)
+img_rows, img_cols = 28, 28
 
-#fix values in y
-#left 9 : 1
-#right 10 : 2
-#nothing  : 0
-#hit   11 : 3
-for yyy in range(0,y_all.shape[0]):
-    if y_all[yyy] == 9:
-        y_all[yyy] = 1
-    elif y_all[yyy] == 10:
-        y_all[yyy] = 2
-    elif y_all[yyy] == 11:
-        y_all[yyy] = 3
+# the data, shuffled and split between train and test sets
+# the data is originally in greyscale
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_all_flat = x_all.reshape(n_images, rows*columns*3)
-x_train, x_test = x_all_flat[:10000,], x_all_flat[10000:,]
-y_train, y_test = y_all[:10000], y_all[10000:]
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+print('x_train shape:', x_train.shape)
+# shape (no samples, no rows, no columns,greyscale dimension)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-#%%
 # convert class vectors to binary class matrices
-y_train2 = keras.utils.to_categorical(y_train, num_classes)
-y_test2 = keras.utils.to_categorical(y_test, num_classes)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-# load model
-model=load_model('model1_fighting.h5')
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
 
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
 
-# show 1st image 
-cv2.imwrite('sample.png',x_train[0].reshape(56,56,3)*255)
+from keras.preprocessing.image import ImageDataGenerator
+datagen = ImageDataGenerator(
+    featurewise_center=False,               # set input mean to 0 over the dataset
+    samplewise_center=False,                # set each sample mean to 0
+    featurewise_std_normalization=False,    # divide inputs by std of the dataset
+    samplewise_std_normalization=False,     # divide each input by its std
+    zca_whitening=False,                    # apply ZCA whitening
+    rotation_range=5,                       # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.1,                  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.1,                 # randomly shift images vertically (fraction of total height)
+    horizontal_flip=False,                   # randomly flip images
+    vertical_flip=False)                     # randomly flip images
 
-#%% Heatmaps 2
+datagen.fit(x_train)
+
+model.fit_generator(datagen.flow(x_train, y_train, shuffle=True), 
+          epochs=epochs,
+          verbose=1,
+          steps_per_epoch=1,          
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#%% Heatmaps
 import numpy as np, matplotlib.pyplot as plt, skimage.exposure, skimage.color, skimage.filters, tqdm
 def hide_axes(ax): ax.set_xticks([]), ax.set_yticks([])
-class_index = {str(i): str(i) for i in range(4)}
-# 4 is for the 4 keystroke classes
+class_index = {str(i): str(i) for i in range(10)}
+# 10 is for digits 0 through 9
 
 def predict(x):
-    # our model.predict requires 1, row*column*3
-    x = x.reshape(1,56*56*3)
+    #1,28,28,1 made greyscale
+    x = np.expand_dims(skimage.color.rgb2gray(x), -1)
     # probabilities of predictions[0]
     return model.predict(x)
 
@@ -131,7 +132,7 @@ class Heatmap:
     def predict_masks(self, im, masks, heatmaps, topclasses):
         for m in tqdm.tqdm(masks):
             prediction = predict(im*self.gray2rgb(m))[0]
-            for c in range(4):
+            for c in range(5):
                 clsnum, clsname = topclasses[c]
                 heatmaps[c] += (prediction[clsnum]*m)
                 
@@ -151,23 +152,23 @@ class Heatmap:
         topclasses = {}
         # for index and value in sorted class labels from 0 to 4
         # print the class label, the prob, and the str class label
-        for i, p in enumerate(predidx[:4]):
+        for i, p in enumerate(predidx[:5]):
             print(p, preds[p], class_index[str(p)])
             topclasses[i] = (p, class_index[str(p)])
         # top class for index is key (index) value (class prediction)    
         print('Top classes: ', topclasses)
         
         # (5,28,28)= (5,) + (28,28)
-        heatmaps = np.zeros((4,) + im.shape[1:3])
+        heatmaps = np.zeros((5,) + im.shape[1:3])
         self.predict_masks(im, masks, heatmaps, topclasses)
         for h in heatmaps: h = h / masknorm
-        fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+        fig, axes = plt.subplots(2, 6, figsize=(10, 5))
         axes[0,0].imshow(img), axes[1,0].imshow(img)        
         axes[0,0].set_title(title)
         hide_axes(axes[0,0]), hide_axes(axes[1,0])       
         predictions = np.sum(heatmaps, axis=(1,2,))
         predictions /= predictions.max()
-        for n, i in enumerate(np.argsort(predictions)[::-1][:4]):
+        for n, i in enumerate(np.argsort(predictions)[::-1][:5]):
             h = ((255 * heatmaps[i])/heatmaps[i].max()).astype('uint16')
             h = skimage.filters.gaussian(h, 0.5)
             h = skimage.exposure.equalize_adapthist(h)    
@@ -187,12 +188,12 @@ class Heatmap:
 # data is not contained in the model
 heatmap = Heatmap(model)
 #%% Show random heatmap
-x_test2=x_test.reshape(2500,56,56,3)
+i = np.random.randint(len(x_test))
 
 # go through first 100 samples
 #
 for i in range(0,10):
     # 28,28,3 (rgb)
-    im = x_test2[i]
+    im = skimage.color.gray2rgb(x_test[i].squeeze())
     # step into the function
     h = heatmap.explain_prediction_heatmap(im, nmasks=(3,5,7,9,11))
