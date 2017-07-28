7410869dbcc737410afadc59c938e2a90339ccf7
Dylan Fontaine <dfontaine002@nam.ad.pwcinternal.com>
2017-07-28 11:42:33 -0500

    deleted old files

--- collect_screencaps.py
+++ collect_screencaps.py
@@ -14,64 +14,55 @@ def format_number(x):
 #%%
 
 #calculate game number and create directory to store images
-folder_list = os.listdir('screencaps')
+folder_list = os.listdir('screencaps_fighting')
 current_max = 0
 if len(folder_list) > 0:
     current_max = int(folder_list[-1][-4:])
 game_number = current_max + 1
-os.makedirs('screencaps/game_' + format_number(game_number))
+os.makedirs('screencaps_fighting/game_' + format_number(game_number))
 
 #wait a few seconds so that user can start game
 print('waiting...')
-time.sleep(5)
+time.sleep(6)
 
 #Capture 600 sequential screenshots
 print('collecting screenshots...')
 img_list = []
 labels = []
-for i in range(0,600):
-    #check which keys are pressed
-    left = win32api.GetAsyncKeyState(37)
-    up = win32api.GetAsyncKeyState(38)
-    right = win32api.GetAsyncKeyState(39)
-    down = win32api.GetAsyncKeyState(40)
-    #assign class label and add to list
-    if down <= -32767:
-        if left <= -32767:
-            my_label = 6
-        elif right <= -32767:
-            my_label = 8
-        else:
-            my_label = 4
-    elif up <= -32767:
-        if left <= -32767:
-            my_label = 5
-        elif right <= -32767:
-            my_label = 7
-        else:
-            my_label = 3
-    elif left <= -32767:
-        my_label = 1
-    elif right <= -32767:
-        my_label = 2
-    else:
-        my_label = 0
-    labels.append(my_label)
+for i in range(0,500):
     #take screenshot and add to list
     img = ImageGrab.grab(bbox=(0,0,1366,768)) 
     img_list.append(img)
+    
+    #check which keys are pressed
+    left = win32api.GetAsyncKeyState(65) # A Key
+    #up = win32api.GetAsyncKeyState(38)
+    right = win32api.GetAsyncKeyState(68) # D key
+    #down = win32api.GetAsyncKeyState(40)
+    #assign class label and add to list
+    jKey = win32api.GetAsyncKeyState(74)
+    if (left <= -32767 or left==1):
+        my_label = 9
+    elif (right <= -32767 or right==1):
+        my_label = 10
+    elif (jKey <= -32767 or jKey==1):
+        my_label = 11
+    else:
+        my_label = 0
+    labels.append(my_label)
+
 print('done collecting. now saving...')
 
 #Save screenshots to 'screencaps' folder
 j=0
 for img in img_list:
     j += 1
-    img_np = np.array(img)
-    img_cropped = img_np[196:668,189:818]
     #frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('screencaps/game_' + format_number(game_number) + '/pic_' + format_number(j) + '.jpg', img_cropped)
+    img_np = np.array(img)
+    img_cropped = img_np[80:730,194:1170]
+    cv2.imwrite('screencaps_fighting/game_' + format_number(game_number) + '/pic_' + format_number(j) + '.jpg', img_cropped)
 #save labels to file
-with open('screencaps/game_' + format_number(game_number) + '/labels.csv', 'w', newline='') as csvfile:
+with open('screencaps_fighting/game_' + format_number(game_number) + '/labels.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     for l in labels:
         writer.writerow([l])
@@ -87,7 +78,12 @@ print('done saving')
 #%%
 #Show an image
 #import matplotlib.pyplot as plt
-#plt.imshow(a_image, cmap = 'gray')
+#time.sleep(3)
+
+#img = np.array(ImageGrab.grab(bbox=(0,0,1366,768)))
+#img_cropped = img[80:730,194:1170]
+
+#plt.imshow(img_cropped, cmap = 'gray')
 
 
 
